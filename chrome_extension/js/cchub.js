51b449a397eb8af08692e3503a9f70343d93bd13
Hubert T <ht335965@students.mimuw.edu.pl>
2014-10-26 01:17:02 +0200

    fix

--- chrome_extension/js/cchub.js
+++ chrome_extension/js/cchub.js
@@ -5,14 +5,36 @@
 
 $(function () {
 
-    $.get("https://students.mimuw.edu.pl/~ht335965/test.json")
+    var url = window.location.href;
+
+    console.log(url);
+
+    var re = /.*github\.com\/([^\/]+)\/([^\/]+)\/pull\/([^/]+)\/.*/;
+    var tokens = url.match(re);
+    var user = tokens[1];
+    var repo = tokens[2];
+    var pull = tokens[3];
+
+    console.log('KURWA', user, repo, pull);
+    console.log(tokens);
+
+    var requestUrl = "http://students.mimuw.edu.pl:8080/client/" + user + "/" + repo + "/" + pull;
+
+    console.log(requestUrl);
+
+    $.get(requestUrl)
         .done(function (result) {
             applyNotifications(result);
+            setUpScrolbarHints();
         })
         .fail(function (errmsg) {
             alert(errmsg);
         });
 });
+
+function setUpScrolbarHints() {
+    $('body').append('<div id="scrollbar-hint"></div>')
+}
 
 function applyNotifications(data) {
     console.log(data);
@@ -21,21 +43,28 @@ function applyNotifications(data) {
 
     files.forEach(function (item) {
         item.notifications.forEach(function (notif) {
-            console.log('filename', item.filename);
-            console.log('notif', notif);
-            console.log('.blob-code b[data-path="' + item.filename + '"][data-line="' + notif.line + '"]');
+//            console.log('filename', item.filename);
+//            console.log('notif', notif);
+//            console.log('.blob-code b[data-path="' + item.filename + '"][data-line="' + notif.line + '"]');
             var b = $('.blob-code b[data-path="' + item.filename + '"][data-line="' + notif.line + '"]');
 
-            b.parent().parent()
-                .addClass('cchub-notification')
-                .addClass('cchub-' + notif.type);
+            b.parent().parent().addClass('cchub-notification');
 
             b.parent()
                 .append(
                     '<div class="cchub-more-btn">' +
-                        '<div class="cchub-icon"></div>' +
-                    '</div>' +
-                    '<div class="cchub-tooltip">' + notif.content + '</div>');
+                        '<div class="cchub-tooltip">' + notif.content + '</div>' +
+                    '</div>'
+            );
+
+//            $('#scrollbar-hint').append('<div class="item" style="' + top + '"></div>')
+
+            notif.type.forEach(function (type) {
+                b.parent().children('.cchub-more-btn:last-child').append(
+                    '<div class="cchub-icon cchub-' + type + '"></div>'
+                );
+                b.parent().parent().addClass('cchub-' + type);
+            });
         });
     });
 }
