1c8c22ceeed41c1f8af9dfdc69ca774894765cc8
Leonid Logvinov <leonid.logvinov@codilime.com>
2014-10-25 22:13:39 +0200

    l rvnlaerno

--- server/YouCheck/YouCheck/views.py
+++ server/YouCheck/YouCheck/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 import urllib2
+from undjango import *
 
 @csrf_exempt
 def get_post(request):
@@ -11,5 +12,12 @@ def get_post(request):
     branch_name = json.dumps(json_data['pull_request']['head']['ref'])
     print diff_url
     print branch_name
-    # "https://github.com/LogvinovLeon/Github_Code_Style/pull/2.diff"
+    pull_request_id = get_pull_request_id(diff_url)
+    full_folder_name = download_all_the_shit(diff_url, branch_name)
+    res = cpp_check(full_folder_name)
+    with open(full_folder_name + "/result.json", "w") as text_file:
+        try:
+            text_file.write(json.dumps(parse_cppcheck_result(res, pull_request_id, full_folder_name), indent=2))
+        except:
+            pass
     return HttpResponse(request)
