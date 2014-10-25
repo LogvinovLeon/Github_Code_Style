d6a3e3a21a54dc28f40c814cfd47aaf5b3316679
Leonid Logvinov <leonid.logvinov@codilime.com>
2014-10-25 22:19:22 +0200

    fixes

--- server/YouCheck/YouCheck/undjango.py
+++ server/YouCheck/YouCheck/undjango.py
@@ -3,53 +3,66 @@ import urllib2
 import re
 import os
 from os.path import expanduser
+import json
 
 
 def parse_diff(diff_url):
     diff = urllib2.urlopen(diff_url).read()
-    #print diff
     diff_list = [match[13:].split(" ")[0] for match in re.findall(r'diff --git .*', diff)]
     return diff_list
-    # print diff
-
 
 def download_all_the_shit(d_url, br_name):
     f_name = '#'.join(d_url.split('/')[-4:])
-    try:
-        os.makedirs(f_name)
-    except OSError as exception:
-        pass
     diff_files = parse_diff(d_url)
-    print diff_files
     full_folder_name = expanduser("~") + "/pull_requests/" + f_name
-    # https://github.com/LogvinovLeon/Github_Code_Style/blob/br2/server/YouCheck/4
     for _file in diff_files:
-
         my_path_file = full_folder_name + "/" + _file
         temp = d_url.split('/')[3:5]
         temp.extend([br_name, _file])
         fi_url = "https://raw.githubusercontent.com/" + '/'.join(temp)
-        print fi_url
+        # print fi_url
         if not os.path.exists(os.path.dirname(my_path_file)):
             if os.path.dirname(my_path_file):
                 os.makedirs(os.path.dirname(my_path_file))
         with open(my_path_file, "w") as text_file:
-            print my_path_file
-            text_file.write(urllib2.urlopen(fi_url).read())
+            # print my_path_file
+            try:
+                text_file.write(urllib2.urlopen(fi_url).read())
+            except:
+                pass
     return full_folder_name
 
 
 def cpp_check(full_folder_name):
-    print full_folder_name
-
     process = subprocess.Popen(
         ["/home/leonid/cppcheck", "--enable=all", "--inconclusive", "--std=posix", full_folder_name + "/*"],
-        stdout=subprocess.PIPE)
-    (output, err) = process.communicate()
-    print output
-    exit_code = process.wait()
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    process.wait()
+    (_, err) = process.communicate()
+    return err.split("\n")
 
 
-diff_url = "https://github.com/LogvinovLeon/Github_Code_Style/pull/4.diff"
-full_folder_name = download_all_the_shit(diff_url, "cpp-files")
-cpp_check(full_folder_name)
+def parse_cppcheck_result(res, id, full_folder_name):
+    ddict = {}
+    for line in res:
+        if len(line) > 0:
+            if line[0:len(full_folder_name) + 1] == "[" + full_folder_name:
+                nline = line[len(full_folder_name) + 2:]
+                phrases = nline.split(":", 2)
+                (_file, ln, tmp) = (phrases[0], phrases[1][:-1], phrases[2][1:])
+                desc = tmp.split(") ", 1)
+                types = desc[0][1:].split(", ")
+                desc = desc[1]
+                noti = {"type": types, "content": desc, "line": ln}
+                if _file in ddict:
+                    ddict[_file].extend([noti])
+                else:
+                    ddict[_file] = [noti]
+    tab = [{"filename": key, "notifications": value} for key, value in ddict.iteritems()]
+    return {"pull": id, "files": tab}
+
+def get_pull_request_id(diff_url):
+    return diff_url.split("/")[-1:][0].split('.')[0]
+
+#diff_url = "https://github.com/LogvinovLeon/Github_Code_Style/pull/6.diff"
+#branch_name = "test"
