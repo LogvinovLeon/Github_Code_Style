daf87bf71197f8dd7b968cdaf3d6e718fdf18e38
Leonid Logvinov <ll345374@students.mimuw.edu.pl>
2014-10-25 17:40:54 +0200

    Getting github notifications

--- server/YouCheck/YouCheck/views.py
+++ server/YouCheck/YouCheck/views.py
@@ -1,4 +1,9 @@
-from django.views.generic import base
-class ServerScoresView(base):
-    def get(self, request):
-        print request
+from django.http import HttpResponse
+
+from django.views.decorators.csrf import csrf_exempt
+import json
+@csrf_exempt
+def get_post(request):
+	json_data = json.loads(request.body)
+	print(json.dumps(json_data['pull_request']['diff_url'], sort_keys=True, indent=4 * ' '))
+	return HttpResponse(request);
