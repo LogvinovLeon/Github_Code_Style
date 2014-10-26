0bafdb60c00a0ab3ae971c8b7db0ec42acc63b04
Hubert Tarasiuk <ht335965@students.mimuw.edu.pl>
2014-10-26 06:29:13 +0100

    fix

--- server/YouCheck/run_astyle.sh
+++ server/YouCheck/run_astyle.sh
@@ -8,16 +8,10 @@ cd $folder_path
 files=$(find -type f -and -not -iname '*result.json' -and -not -iname '*formatting.json')
 
 for file in $files; do
-	echo $file
+	echo $file >&2
 	astyle $file 2>&1 >/dev/null
-#	out=$(diff --unchanged-line-format="#" --old-line-format="&$file:%dn%c'\012'" --new-line-format="^$file:%L" $file ${file}.orig |& sed 's/^#\+/#\n/g' |& sed 's/^\^/\^/g') 
 	diff $file ${file}.orig >&2
-#	echo "${out}" >&2
-	for line in $out; do
-#		[ "$line" == "#" ] && echo $line >&2 || echo ${line#?} 1>&2
-		:
-	done
-	[ -f ${file}.orig ] && rm $file && mv ${file}.orig $file 2>/dev/null
+	([ -f ${file}.orig ] && rm $file && mv ${file}.orig $file) 2>&1 >/dev/null
 done
 
 cd -
