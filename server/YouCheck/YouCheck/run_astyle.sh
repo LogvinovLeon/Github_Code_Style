d836c0d2f0cd7019e3ad3a6c9198e1feea1504b6
Hubert Tarasiuk <ht335965@students.mimuw.edu.pl>
2014-10-26 02:56:39 +0100

    run_astyle.sh

---
+++ server/YouCheck/YouCheck/run_astyle.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+[ -n "$1" ] || exit 1
+
+folder_path=$1
+cd $folder_path
+
+files=$(find -type f -and -not -iname '*result.json')
+
+for file in $files; do
+	echo $file
+	astyle $file 2>&1 >/dev/null
+	diff --unchanged-line-format="#" --old-line-format="$file:%dn%c'\012'" --new-line-format="" $file ${file}.orig |& sed 's/#\+/#\n/g'
+	[ -f ${file}.orig ] && rm $file && mv ${file}.orig $file 2>/dev/null
+done
+
+cd -
